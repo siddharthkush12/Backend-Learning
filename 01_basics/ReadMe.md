7c7a5dcb5a72745754c7d256363608b67126c32a
siddharthkush12 <siddharth5044549@gmail.com>
2025-02-19 07:31:31 +0530

    push

---
+++ 01_basics/ReadMe.md
@@ -0,0 +1,4 @@
+1:- npm init   (initialize package.json)
+2:- add in package.json  ("type":"module")  to overcome warnings
+3:- npm install express     (install express)
+4:- .env (npm i dotenv)     we dont want to deploy senstive informations like port,keys etc
--- 01_basics/index.js
+++ 01_basics/index.js
@@ -1,1 +1,27 @@
-console.log("HI");
+import express from 'express'
+import 'dotenv/config'
+const app = express()
+const port=4000
+fafdaf
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/user',(req,res)=>{
+    res.send("Siddharth Kushwaha")
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>Please Login</h1>')
+})
+
+app.get('/youtube',(req,res)=>{
+    res.send('youtube')
+})
+app.get('/youtube2',(req,res)=>{
+    res.send('youtube2')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
